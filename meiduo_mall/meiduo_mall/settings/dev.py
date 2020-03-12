5190e1d42a421d3ff3b1fd31e8dda67236d251e6
APGLn <584647996@qq.com>
2020-03-12 13:21:18 +0800

    测试一下合并

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+# 重点记住
 import os  # 操作系统ubuntu模块
 import sys  # python模块
 
